2541d2170a9668575e1eb6224917cfaa05291395
nuva <nuvaragan@gmail.com>
2025-12-10 14:35:39 +0100

    reponse q1

---
+++ q1_v2_enseash.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+
+#define BIENVENUE_MSG "Bienvenue dans le Shell ENSEA.\n\rPour quitter taper 'exit'\n"
+#define PROMPT "enseash %\n"
+
+int main()
+{
+
+    write(STDOUT_FILENO, BIENVENUE_MSG, strlen(BIENVENUE_MSG));
+    write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+
+    return 0;
+}
